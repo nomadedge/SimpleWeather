2d5dc8a077581ebef8abe0adc0b847569e082ea2
nomadedge <nomad.edge@gmail.com>
2019-10-01 21:27:36 +0300

    Cleaunp some usings, update nuget packages

--- SimpleWeather.Web/Bases/Render.cs
+++ SimpleWeather.Web/Bases/Render.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SimpleWeather.Web.Models;
 using SimpleWeather.Web.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SimpleWeather.Web.Bases
 {
--- SimpleWeather.Web/Models/Weather.cs
+++ SimpleWeather.Web/Models/Weather.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SimpleWeather.Web.Models
+﻿namespace SimpleWeather.Web.Models
 {
     public class Weather
     {
